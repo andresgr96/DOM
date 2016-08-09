8c8b666db193a7edfdd7175df3d422c7e31057fe
Andres Garcia and jake blank <andres@>
2016-08-09 14:49:02 -0400

    Update DOM files with adding images to buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").append("<li>Why hello there!</li>");
   });
-
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").append("<li>Goodbye, dear user!</li>");
@@ -12,5 +11,9 @@ $(document).ready(function() {
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").append("<li>Pardon me. I meant no offense.</li>");
+  });
+  $("button#image").click(function() {
+    $("ul#webpage").append("<img src='img/one.jpg' width='200' height='200'>");
+
   });
 });
