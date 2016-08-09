45d1b4b50071198581be7357e4eeae474e1ab6e2
Andres Garcia and jake blank <andres@>
2016-08-09 13:44:21 -0400

    Add append functions.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").append("<li>Why hello there!</li>");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").append("<li>Goodbye, dear user!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").append("<li>Pardon me. I meant no offense.</li>");
+  });
+});
